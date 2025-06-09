2074f7936ba0ce6183fd50b216fae6331d8194cd
asdfd5465 <forfirestick25@gmail.com>
2025-06-09 15:32:31 +0300

    Update CategoryDao.kt

--- app/src/main/java/bankwiser/bankpromotion/material/data/local/dao/CategoryDao.kt
+++ app/src/main/java/bankwiser/bankpromotion/material/data/local/dao/CategoryDao.kt
@@ -1,12 +1,17 @@
 package bankwiser.bankpromotion.material.data.local.dao
 
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import bankwiser.bankpromotion.material.data.local.entity.CategoryEntity
 import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface CategoryDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertAll(categories: List<CategoryEntity>)
+
     @Query("SELECT * FROM Categories ORDER BY category_name ASC")
     fun getAllCategories(): Flow<List<CategoryEntity>>
 }
