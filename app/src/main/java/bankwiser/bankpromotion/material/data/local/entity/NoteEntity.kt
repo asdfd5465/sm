af4e4f9b9d2279a36afd0a152c25fa218d91411c
asdfd5465 <forfirestick25@gmail.com>
2025-06-09 14:04:08 +0300

    Create NoteEntity.kt

---
+++ app/src/main/java/bankwiser/bankpromotion/material/data/local/entity/NoteEntity.kt
@@ -0,0 +1,14 @@
+package bankwiser.bankpromotion.material.data.local.entity
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+// Simplified for Phase 2
+@Entity(tableName = "Notes")
+data class NoteEntity(
+    @PrimaryKey @ColumnInfo(name = "note_id") val noteId: String,
+    @ColumnInfo(name = "title") val title: String,
+    @ColumnInfo(name = "body") val body: String,
+    @ColumnInfo(name = "sub_category_id") val subCategoryId: String
+)
