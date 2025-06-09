c8577ce2448727f9da96948ad2215299f5dc7c04
asdfd5465 <forfirestick25@gmail.com>
2025-06-09 16:18:03 +0300

    Update BankWiserApplication.kt

--- app/src/main/java/bankwiser/bankpromotion/material/BankWiserApplication.kt
+++ app/src/main/java/bankwiser/bankpromotion/material/BankWiserApplication.kt
@@ -2,9 +2,15 @@ package bankwiser.bankpromotion.material
 
 import android.app.Application
 import bankwiser.bankpromotion.material.data.local.ContentDatabase
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.SupervisorJob
 
 class BankWiserApplication : Application() {
+    // Create an application-wide scope for coroutines.
+    val applicationScope = CoroutineScope(SupervisorJob())
+
+    // Use 'lazy' to initialize the database only when first accessed.
     val contentDatabase: ContentDatabase by lazy {
-        ContentDatabase.getDatabase(this)
+        ContentDatabase.getDatabase(this, applicationScope)
     }
 }
