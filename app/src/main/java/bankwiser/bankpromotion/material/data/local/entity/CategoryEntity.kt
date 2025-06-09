f979e5e585c2f0231f67a286b0efcb770fc49fb0
asdfd5465 <forfirestick25@gmail.com>
2025-06-09 11:47:31 +0300

    Create CategoryEntity.kt

---
+++ app/src/main/java/bankwiser/bankpromotion/material/data/local/entity/CategoryEntity.kt
@@ -0,0 +1,11 @@
+package bankwiser.bankpromotion.material.data.local.entity
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "Categories")
+data class CategoryEntity(
+    @PrimaryKey @ColumnInfo(name = "category_id") val categoryId: String,
+    @ColumnInfo(name = "category_name") val categoryName: String
+)
