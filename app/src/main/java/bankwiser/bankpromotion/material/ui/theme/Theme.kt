8652adf48cd66cdb93fe8b5f422f30c6302a250c
asdfd5465 <forfirestick25@gmail.com>
2025-06-09 13:01:18 +0300

    Update Theme.kt

--- app/src/main/java/bankwiser/bankpromotion/material/ui/theme/Theme.kt
+++ app/src/main/java/bankwiser/bankpromotion/material/ui/theme/Theme.kt
@@ -7,6 +7,7 @@ import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
@@ -51,7 +52,7 @@ fun BankWiserProTheme(
         SideEffect {
             val window = (view.context as Activity).window
             window.statusBarColor = colorScheme.primary.toArgb()
-            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = !darkTheme
         }
     }
 
