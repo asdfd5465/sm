8d6b4f1a4c50b6e5b017d93c19e446ff29f87f13
asdfd5465 <forfirestick25@gmail.com>
2025-06-09 15:19:10 +0300

    Update CategoryEntity.kt

--- app/src/main/java/bankwiser/bankpromotion/material/data/local/entity/CategoryEntity.kt
+++ app/src/main/java/bankwiser/bankpromotion/material/data/local/entity/CategoryEntity.kt
@@ -6,6 +6,7 @@ import androidx.room.PrimaryKey
 
 @Entity(tableName = "Categories")
 data class CategoryEntity(
+    // It's unusual for a Primary Key to be nullable, but this matches your DB schema.
     @PrimaryKey @ColumnInfo(name = "category_id") val categoryId: String,
-    @ColumnInfo(name = "category_name") val categoryName: String? // Changed from String
+    @ColumnInfo(name = "category_name") val categoryName: String?
 )
