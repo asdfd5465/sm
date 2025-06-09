90be78ff168b14cdf9fcecf987182562691d064b
asdfd5465 <forfirestick25@gmail.com>
2025-06-09 11:48:28 +0300

    Create ContentDatabase.kt

---
+++ app/src/main/java/bankwiser/bankpromotion/material/data/local/ContentDatabase.kt
@@ -0,0 +1,36 @@
+package bankwiser.bankpromotion.material.data.local
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import bankwiser.bankpromotion.material.data.local.dao.CategoryDao
+import bankwiser.bankpromotion.material.data.local.entity.CategoryEntity
+
+@Database(entities = [CategoryEntity::class], version = 1, exportSchema = false)
+abstract class ContentDatabase : RoomDatabase() {
+
+    abstract fun categoryDao(): CategoryDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: ContentDatabase? = null
+        private const val DATABASE_NAME = "content.db"
+        private const val ASSET_DB_PATH = "database/content_v1.db"
+
+        fun getDatabase(context: Context): ContentDatabase {
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    ContentDatabase::class.java,
+                    DATABASE_NAME
+                )
+                .createFromAsset(ASSET_DB_PATH)
+                .build()
+
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
+}
