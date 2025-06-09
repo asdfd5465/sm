0f40ba523c1895daba56f2406fe84b23b2ffacb2
asdfd5465 <forfirestick25@gmail.com>
2025-06-09 12:13:10 +0300

    Update Color.kt

--- app/src/main/java/bankwiser/bankpromotion/material/ui/theme/Color.kt
+++ app/src/main/java/bankwiser/bankpromotion/material/ui/theme/Color.kt
@@ -2,10 +2,9 @@ package bankwiser.bankpromotion.material.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val PrimaryBlue = Color(0xFF4C51BF)
+val PrimaryDark = Color(0xFF434190)
+val GradientEnd = Color(0xFF6B46C1)
+val TextPrimary = Color(0xFF2D3748)
+val TextSecondary = Color(0xFF4A5568)
+val BackgroundLight = Color(0xFFF7FAFC)
