2cbd5d5af812314362c1b58352ff52107602106f
asdfd5465 <forfirestick25@gmail.com>
2025-06-09 15:33:24 +0300

    Update NoteDao.kt

--- app/src/main/java/bankwiser/bankpromotion/material/data/local/dao/NoteDao.kt
+++ app/src/main/java/bankwiser/bankpromotion/material/data/local/dao/NoteDao.kt
@@ -1,12 +1,17 @@
 package bankwiser.bankpromotion.material.data.local.dao
 
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import bankwiser.bankpromotion.material.data.local.entity.NoteEntity
 import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface NoteDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertAll(notes: List<NoteEntity>)
+
     @Query("SELECT * FROM Notes WHERE sub_category_id = :subCategoryId")
     fun getNotesForSubCategory(subCategoryId: String): Flow<List<NoteEntity>>
 
