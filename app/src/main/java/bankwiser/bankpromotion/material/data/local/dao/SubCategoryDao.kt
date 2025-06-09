90da5a2689524d20e201792b11360050abc2d752
asdfd5465 <forfirestick25@gmail.com>
2025-06-09 15:32:56 +0300

    Update SubCategoryDao.kt

--- app/src/main/java/bankwiser/bankpromotion/material/data/local/dao/SubCategoryDao.kt
+++ app/src/main/java/bankwiser/bankpromotion/material/data/local/dao/SubCategoryDao.kt
@@ -1,12 +1,17 @@
 package bankwiser.bankpromotion.material.data.local.dao
 
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import bankwiser.bankpromotion.material.data.local.entity.SubCategoryEntity
 import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface SubCategoryDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertAll(subCategories: List<SubCategoryEntity>)
+
     @Query("SELECT * FROM SubCategories WHERE category_id = :categoryId ORDER BY sub_category_name ASC")
     fun getSubCategoriesForCategory(categoryId: String): Flow<List<SubCategoryEntity>>
 }
