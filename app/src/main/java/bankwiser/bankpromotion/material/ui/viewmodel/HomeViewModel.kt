0b819371c58e833217347f2211e2620586a57f76
asdfd5465 <forfirestick25@gmail.com>
2025-06-10 12:54:49 +0300

    Update HomeViewModel.kt

--- app/src/main/java/bankwiser/bankpromotion/material/ui/viewmodel/HomeViewModel.kt
+++ app/src/main/java/bankwiser/bankpromotion/material/ui/viewmodel/HomeViewModel.kt
@@ -2,17 +2,27 @@ package bankwiser.bankpromotion.material.ui.viewmodel
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import bankwiser.bankpromotion.material.data.local.entity.CategoryEntity
+import bankwiser.bankpromotion.material.data.model.Category
 import bankwiser.bankpromotion.material.data.repository.ContentRepository
-import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
-class HomeViewModel(repository: ContentRepository) : ViewModel() {
-    val categories: StateFlow<List<CategoryEntity>> = repository.getAllCategories()
-        .stateIn(
-            scope = viewModelScope,
-            started = SharingStarted.WhileSubscribed(5000),
-            initialValue = emptyList()
-        )
+class HomeViewModel(private val repository: ContentRepository) : ViewModel() {
+    private val _categories = MutableStateFlow<List<Category>>(emptyList())
+    val categories: StateFlow<List<Category>> = _categories
+
+    init {
+        loadCategories()
+    }
+
+    private fun loadCategories() {
+        viewModelScope.launch {
+            _categories.value = withContext(Dispatchers.IO) {
+                repository.getAllCategories()
+            }
+        }
+    }
 }
