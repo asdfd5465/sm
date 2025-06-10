1e1a02c6d78c2c6878ad553aacc8782ce762c1b2
asdfd5465 <forfirestick25@gmail.com>
2025-06-10 14:36:33 +0300

    Create GoogleAuthUiClient.kt

---
+++ app/src/main/java/bankwiser/bankpromotion/material/GoogleAuthUiClient.kt
@@ -0,0 +1,87 @@
+package bankwiser.bankpromotion.material
+
+import android.content.Context
+import android.content.Intent
+import android.content.IntentSender
+import com.google.android.gms.auth.api.identity.BeginSignInRequest
+import com.google.android.gms.auth.api.identity.SignInClient
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.coroutines.tasks.await
+import java.util.concurrent.CancellationException
+
+data class SignInResult(
+    val data: UserData?,
+    val errorMessage: String?
+)
+
+data class UserData(
+    val userId: String,
+    val username: String?,
+    val profilePictureUrl: String?,
+    val idToken: String
+)
+
+class GoogleAuthUiClient(
+    private val context: Context,
+    private val oneTapClient: SignInClient
+) {
+    private val auth = FirebaseAuth.getInstance()
+
+    suspend fun signIn(): IntentSender? {
+        val result = try {
+            oneTapClient.beginSignIn(
+                buildSignInRequest()
+            ).await()
+        } catch(e: Exception) {
+            e.printStackTrace()
+            if(e is CancellationException) throw e
+            null
+        }
+        return result?.pendingIntent?.intentSender
+    }
+
+    suspend fun signInWithIntent(intent: Intent): SignInResult {
+        val credential = oneTapClient.getSignInCredentialFromIntent(intent)
+        val idToken = credential.googleIdToken
+        if(idToken == null) {
+            return SignInResult(
+                data = null,
+                errorMessage = "No ID token!"
+            )
+        }
+        
+        return SignInResult(
+            data = UserData(
+                userId = credential.id,
+                username = credential.displayName,
+                profilePictureUrl = credential.profilePictureUri?.toString(),
+                idToken = idToken
+            ),
+            errorMessage = null
+        )
+    }
+
+    suspend fun signOut() {
+        try {
+            auth.signOut()
+            oneTapClient.signOut().await()
+        } catch(e: Exception) {
+            e.printStackTrace()
+            if(e is CancellationException) throw e
+        }
+    }
+
+    private fun buildSignInRequest(): BeginSignInRequest {
+        return BeginSignInRequest.builder()
+            .setGoogleIdTokenRequestOptions(
+                BeginSignInRequest.GoogleIdTokenRequestOptions.builder()
+                    .setSupported(true)
+                    // Your server client ID, which you can get from your Firebase project settings
+                    .setServerClientId(context.getString(R.string.default_web_client_id))
+                    .setFilterByAuthorizedAccounts(false)
+                    .build()
+            )
+            .setAutoSelectEnabled(true)
+            .build()
+    }
+}
