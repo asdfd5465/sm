74ccd353facbfab8364866a7cf582929d196bced
asdfd5465 <forfirestick25@gmail.com>
2025-06-10 12:01:16 +0300

    Update BankWiserApplication.kt

--- app/src/main/java/bankwiser/bankpromotion/material/BankWiserApplication.kt
+++ app/src/main/java/bankwiser/bankpromotion/material/BankWiserApplication.kt
@@ -2,23 +2,9 @@ package bankwiser.bankpromotion.material
 
 import android.app.Application
 import bankwiser.bankpromotion.material.data.local.ContentDatabase
-import bankwiser.bankpromotion.material.data.repository.ContentRepository
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
 
 class BankWiserApplication : Application() {
-
-    // Using by lazy ensures the database and repository are only created when they're first needed
-    val contentDatabase: ContentDatabase by lazy { ContentDatabase.getDatabase(this) }
-    val contentRepository: ContentRepository by lazy { ContentRepository(contentDatabase, this) }
-
-    override fun onCreate() {
-        super.onCreate()
-        // We launch a coroutine to do the one-time database population check
-        // This won't block the main thread
-        CoroutineScope(Dispatchers.IO).launch {
-            contentRepository.checkAndPrepopulate()
-        }
+    val contentDatabase: ContentDatabase by lazy {
+        ContentDatabase.getDatabase(this)
     }
 }
