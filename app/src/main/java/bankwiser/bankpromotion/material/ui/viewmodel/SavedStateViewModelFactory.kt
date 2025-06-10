aa1d73b36f780784c101a9b090a2c43775c0612a
asdfd5465 <forfirestick25@gmail.com>
2025-06-10 13:00:27 +0300

    Create SavedStateViewModelFactory.kt

---
+++ app/src/main/java/bankwiser/bankpromotion/material/ui/viewmodel/SavedStateViewModelFactory.kt
@@ -0,0 +1,26 @@
+package bankwiser.bankpromotion.material.ui.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
+import androidx.lifecycle.viewmodel.CreationExtras
+import bankwiser.bankpromotion.material.data.repository.ContentRepository
+
+class SavedStateViewModelFactory(private val repository: ContentRepository) : ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>, extras: CreationExtras): T {
+        val savedStateHandle = extras.createSavedStateHandle()
+        if (modelClass.isAssignableFrom(SubCategoryViewModel::class.java)) {
+            @Suppress("UNCHECKED_CAST")
+            return SubCategoryViewModel(repository, savedStateHandle) as T
+        }
+        if (modelClass.isAssignableFrom(NoteListViewModel::class.java)) {
+            @Suppress("UNCHECKED_CAST")
+            return NoteListViewModel(repository, savedStateHandle) as T
+        }
+        if (modelClass.isAssignableFrom(NoteDetailViewModel::class.java)) {
+            @Suppress("UNCHECKED_CAST")
+            return NoteDetailViewModel(repository, savedStateHandle) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class: ${modelClass.name}")
+    }
+}
