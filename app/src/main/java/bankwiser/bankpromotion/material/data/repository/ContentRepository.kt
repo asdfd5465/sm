3d6445802c0acdaf90a9fb981e20a4f6f0f90bfa
asdfd5465 <forfirestick25@gmail.com>
2025-06-10 12:54:09 +0300

    Update ContentRepository.kt

--- app/src/main/java/bankwiser/bankpromotion/material/data/repository/ContentRepository.kt
+++ app/src/main/java/bankwiser/bankpromotion/material/data/repository/ContentRepository.kt
@@ -1,12 +1,10 @@
 package bankwiser.bankpromotion.material.data.repository
 
-import bankwiser.bankpromotion.material.data.local.ContentDatabase
+import bankwiser.bankpromotion.material.data.local.DatabaseHelper
 
-class ContentRepository(private val db: ContentDatabase) {
-
-    // Standard data access functions
-    fun getAllCategories() = db.categoryDao().getAllCategories()
-    fun getSubCategories(categoryId: String) = db.subCategoryDao().getSubCategoriesForCategory(categoryId)
-    fun getNotes(subCategoryId: String) = db.noteDao().getNotesForSubCategory(subCategoryId)
-    fun getNote(noteId: String) = db.noteDao().getNoteById(noteId)
+class ContentRepository(private val dbHelper: DatabaseHelper) {
+    fun getAllCategories() = dbHelper.getAllCategories()
+    fun getSubCategories(categoryId: String) = dbHelper.getSubCategories(categoryId)
+    fun getNotes(subCategoryId: String) = dbHelper.getNotes(subCategoryId)
+    fun getNote(noteId: String) = dbHelper.getNote(noteId)
 }
