8288b50babff24acfbb4f0fb3d6ec7e9a8f726f0
asdfd5465 <forfirestick25@gmail.com>
2025-06-10 13:00:01 +0300

    Create ViewModelFactory.kt

---
+++ app/src/main/java/bankwiser/bankpromotion/material/ui/viewmodel/ViewModelFactory.kt
@@ -0,0 +1,15 @@
+package bankwiser.bankpromotion.material.ui.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import bankwiser.bankpromotion.material.data.repository.ContentRepository
+
+class ViewModelFactory(private val repository: ContentRepository) : ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(HomeViewModel::class.java)) {
+            @Suppress("UNCHECKED_CAST")
+            return HomeViewModel(repository) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}
