61f400d0ba1598dbd273a5d1197354771a7b00fa
asdfd5465 <forfirestick25@gmail.com>
2025-06-10 11:59:51 +0300

    Update NoteEntity.kt

--- app/src/main/java/bankwiser/bankpromotion/material/data/local/entity/NoteEntity.kt
+++ app/src/main/java/bankwiser/bankpromotion/material/data/local/entity/NoteEntity.kt
@@ -4,10 +4,12 @@ import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-@Entity(tableName = "Notes")
+// Reflects the new DB schema for Phase 2
+@Entity(tableName = "notes")
 data class NoteEntity(
     @PrimaryKey @ColumnInfo(name = "note_id") val noteId: String,
-    @ColumnInfo(name = "title") val title: String?,
-    @ColumnInfo(name = "body") val body: String?,
+    @ColumnInfo(name = "title") val title: String,
+    @ColumnInfo(name = "body") val body: String,
+    // sub_category_id can be null in your new schema
     @ColumnInfo(name = "sub_category_id") val subCategoryId: String?
 )
