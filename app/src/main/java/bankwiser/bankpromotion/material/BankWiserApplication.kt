52da57a8a9ba3e93abd373752670e78ef5621120
asdfd5465 <forfirestick25@gmail.com>
2025-06-10 12:13:40 +0300

    Update BankWiserApplication.kt

--- app/src/main/java/bankwiser/bankpromotion/material/BankWiserApplication.kt
+++ app/src/main/java/bankwiser/bankpromotion/material/BankWiserApplication.kt
@@ -2,9 +2,16 @@ package bankwiser.bankpromotion.material
 
 import android.app.Application
 import bankwiser.bankpromotion.material.data.local.ContentDatabase
+import bankwiser.bankpromotion.material.data.repository.ContentRepository
 
 class BankWiserApplication : Application() {
+    // Expose a lazy-initialized instance of the database
     val contentDatabase: ContentDatabase by lazy {
         ContentDatabase.getDatabase(this)
+    }
+
+    // Expose a lazy-initialized instance of the repository
+    val contentRepository: ContentRepository by lazy {
+        ContentRepository(contentDatabase)
     }
 }
