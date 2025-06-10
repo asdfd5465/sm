91ff0b3ac29f348fa4febabe9905222e680398a4
asdfd5465 <forfirestick25@gmail.com>
2025-06-10 12:08:11 +0300

    Update CategoryEntity.kt

--- app/src/main/java/bankwiser/bankpromotion/material/data/local/entity/CategoryEntity.kt
+++ app/src/main/java/bankwiser/bankpromotion/material/data/local/entity/CategoryEntity.kt
@@ -7,5 +7,6 @@ import androidx.room.PrimaryKey
 @Entity(tableName = "categories")
 data class CategoryEntity(
     @PrimaryKey @ColumnInfo(name = "category_id") val categoryId: String,
+    // Corrected: Changed from String? to String to match NOT NULL constraint
     @ColumnInfo(name = "category_name") val categoryName: String
 )
