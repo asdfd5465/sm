077613fd9c7c66853f360ffdb3d6b35ef168f415
asdfd5465 <forfirestick25@gmail.com>
2025-06-11 15:30:59 +0300

    Update MainActivity.kt

--- app/src/main/java/bankwiser/bankpromotion/material/MainActivity.kt
+++ app/src/main/java/bankwiser/bankpromotion/material/MainActivity.kt
@@ -19,7 +19,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    AppNavigation()
+                    AppNavigation() // AppNavigation now takes AuthViewModel by default
                 }
             }
         }
